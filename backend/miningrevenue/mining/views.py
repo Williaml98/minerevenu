a0736bc150b7802a587614ace4f69b9c85bc6e31
KABANOFESTO <kabanofesto1@gmail.com>
2026-02-19 14:45:03 +0200

    ch:protect mines routes

--- backend/miningrevenue/mining/views.py
+++ backend/miningrevenue/mining/views.py
@@ -1,3 +1,4 @@
+from rest_framework import generics, permissions, filters, status
 from rest_framework import viewsets
 from .models import Mine, ProductionRecord
 from .serializers import MineSerializer, ProductionRecordSerializer
@@ -6,8 +7,12 @@ from .serializers import MineSerializer, ProductionRecordSerializer
 class MineViewSet(viewsets.ModelViewSet):
     queryset = Mine.objects.all()
     serializer_class = MineSerializer
-
+    permission_classes = [permissions.IsOfficer]
+    
+    
 
 class ProductionRecordViewSet(viewsets.ModelViewSet):
     queryset = ProductionRecord.objects.all()
     serializer_class = ProductionRecordSerializer
+    permission_classes = [permissions.IsOfficer]
+    
