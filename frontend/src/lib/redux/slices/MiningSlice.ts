34ab1d7d2d4d8314b564fd3bccdc68cf43f89075
KABANOFESTO <kabanofesto1@gmail.com>
2026-02-24 10:28:00 +0200

    ch:add Mining slices

---
+++ frontend/src/lib/redux/slices/MiningSlice.ts
@@ -0,0 +1,84 @@
+import { apiSlice } from "./ApiSlice";
+
+const miningApi = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        createMineCompany: builder.mutation({
+            query: (data) => ({
+                url: "mining/mines/",
+                method: "POST",
+                body: data,
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('access')}`,
+                },
+            }),
+        }),
+        createProductionRecord: builder.mutation({
+            query: (data) => ({
+                url: "mining/production/",
+                method: "POST",
+                body: data,
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('access')}`,
+                },
+            }),
+        }),
+
+        createSalesTransaction: builder.mutation({
+            query: (data) => ({
+                url: "mining/production/",
+                method: "POST",
+                body: data,
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('access')}`,
+                },
+            }),
+        }),
+        generateForecast: builder.mutation({
+            query: (data) => ({
+                url: "analytics/generate-forecast/",
+                method: "POST",
+                body: data,
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('access')}`,
+                },
+            }),
+        }),
+        getMineCompanies: builder.query({
+            query: () => ({
+                url: "mining/mines/",
+                method: "GET",
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('access')}`,
+                },
+            }),
+        }),
+        getProductionRecords: builder.query({
+            query: () => ({
+                url: "mining/production/",
+                method: "GET",
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('access')}`,
+                },
+            }),
+        }),
+        getSalesTransactions: builder.query({
+            query: () => ({
+                url: "mining/sales/",
+                method: "GET",
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('access')}`,
+                },
+            }),
+        }),
+    }),
+});
+
+export const {
+    useCreateMineCompanyMutation,
+    useCreateProductionRecordMutation,
+    useCreateSalesTransactionMutation,
+    useGenerateForecastMutation,
+    useGetMineCompaniesQuery,
+    useGetProductionRecordsQuery,
+    useGetSalesTransactionsQuery,
+} = miningApi;
